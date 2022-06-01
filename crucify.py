736e59545042c46e5c3d70c25053a3caf5f9acd9
Mikhail <jackglowpetux@gmail.com>
2022-06-01 20:55:32 +0500

    move savecache inside class

--- crucify.py
+++ crucify.py
@@ -21,7 +21,6 @@ print("Gamemode set to:", gamemode)
 print("Please wait until the maps are loaded...")
 MapCollector = utilities.MapCollector()
 MapCollector.Collect()
-MapCollector.CacheSave()
 
 Game = utilities.Game([args.width, args.height],
                       gamemode, isBorderless=args.borderless)
