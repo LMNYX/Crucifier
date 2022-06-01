48ce20b8ee7c60bc09b603521a3faa70a82c805b
Mikhail <jackglowpetux@gmail.com>
2022-06-02 02:11:21 +0500

    Move again.

--- crucify.py
+++ crucify.py
@@ -19,8 +19,6 @@ gamemode = utilities.Gamemode(
 print("Gamemode set to:", gamemode)
 
 print("Please wait until the maps are loaded...")
-MapCollector = utilities.MapCollector()
-MapCollector.Collect()
 
 Game = utilities.Game([args.width, args.height],
                       gamemode, isBorderless=args.borderless)
