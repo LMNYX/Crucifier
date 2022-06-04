133564ec0191e91fab839d900568e0c8bb9239e7
Sheepposu <49356627+Sheepposu@users.noreply.github.com>
2022-06-03 23:54:17 -0400

    fps argument

--- crucify.py
+++ crucify.py
@@ -8,6 +8,7 @@ parser.add_argument('--gamemode', '-g', type=int, nargs='+',
                     help='gamemode that is supposed to be played')
 parser.add_argument('--width', '-sw', type=int, default=600)
 parser.add_argument('--height', '-sh', type=int, default=480)
+parser.add_argument("--fps", '-f', type=int, default=960)
 parser.add_argument('--borderless', '-b', action='store_true')
 parser.add_argument('--no-cache', '-nc', action='store_true')
 args = parser.parse_args()
@@ -20,6 +21,7 @@ print("Gamemode set to:", gamemode)
 
 print("Please wait until the maps are loaded...")
 
-game = Game([args.width, args.height],
-            gamemode, is_borderless=args.borderless, is_caching_enabled=not args.no_cache)
+game = Game([args.width, args.height], args.fps, gamemode,
+            is_borderless=args.borderless,
+            is_caching_enabled=not args.no_cache)
 game.run()
