6a52898eee82cbe503c009b28fee2810ea5dc28e
Sheepposu <49356627+Sheepposu@users.noreply.github.com>
2022-07-07 03:13:08 -0400

    no longer need Gamemode enum

--- enums.py
+++ enums.py
@@ -1,13 +1,6 @@
 from enum import Enum
 
 
-class Gamemode(Enum):
-    STD = 0
-    MANIA = 1
-    TAIKO = 2
-    CTB = 3  # NOBODY GIVES A FUCK ABOUT IT.
-
-
 class DebugMode(Enum):
     NO = 0
     FEW = 1
