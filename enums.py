5c9f32b1af73037b3505b85f051ba4b2c6fd7e20
Sheepposu <49356627+Sheepposu@users.noreply.github.com>
2022-07-11 00:20:58 -0400

    SkinOption enum

--- enums.py
+++ enums.py
@@ -1,7 +1,11 @@
-from enum import Enum
+from enum import Enum, IntEnum
 
 
 class DebugMode(Enum):
     NO = 0
     FEW = 1
     FULL = 2
+
+
+class SkinOption(IntEnum):
+    CURRENT_COMBO_COLOR = 0
